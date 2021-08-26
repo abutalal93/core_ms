d30dc7ad51000ef63dc66da2469ce304aeb35428
user <mamounalsalman691@gmail.com>
2021-08-26 14:05:09 +0300

    update

--- src/main/java/restaurant/ms/core/repositories/RestaurantRepo.java
+++ src/main/java/restaurant/ms/core/repositories/RestaurantRepo.java
@@ -2,6 +2,7 @@ package restaurant.ms.core.repositories;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import restaurant.ms.core.dto.responses.PageRs;
 import restaurant.ms.core.entities.Restaurant;
@@ -14,6 +15,7 @@ public interface RestaurantRepo extends CrudRepository<Restaurant,String> {
 
     public Restaurant findRestaurantById(Long id);
 
+    @Query("select rest from Restaurant rest where rest.status <> 'DELETED'")
     Page<Restaurant> findAllBy(Pageable pageable);
 
 }
